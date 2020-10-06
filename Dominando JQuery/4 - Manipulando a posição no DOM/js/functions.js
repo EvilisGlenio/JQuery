e26c851d673c5cf7eeb9758a062985d8d3c7399b
EvilisGlenio <glenio.developer@gmail.com>
2020-10-06 16:12:06 -0300

    ...
    ...

--- Dominando JQuery/4 - Manipulando a posição no DOM/js/functions.js
+++ Dominando JQuery/4 - Manipulando a posição no DOM/js/functions.js
@@ -1,5 +1,7 @@
 $(function(){
 
-    
+    $('.box div').append('<span>'+"Elemento adcionado dinamicamente"+'</span>');
+
+    $('.box div').eq(0).append('<h3>'+"Outro elemento adcionado dinamicamente"+'</h3>');
 
 });
