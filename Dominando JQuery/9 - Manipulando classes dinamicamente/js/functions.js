9871027649599760d257577b714688586e95c5f6
EvilisGlenio <glenio.developer@gmail.com>
2020-10-14 16:20:32 -0300

    Aula 09
    Finalizada

---
+++ Dominando JQuery/9 - Manipulando classes dinamicamente/js/functions.js
@@ -0,0 +1,35 @@
+$(function(){
+    //.ADDCLASS & .REMOVECLASS
+    /*$('.box').addClass('b1');
+
+    $('.b1').css({
+        'width':'30%',
+        'paddingTop':'30%',
+        'background':'red'
+    });*/
+
+    /*$('body').click(function(){
+        //Por algum motivo não funcionou
+        //$('.box').removeClass('b1');
+    });*/
+
+
+
+    
+    //.FIND
+    //.find encontra como o nome já auto esplica
+    //$('.box').find('.c2').css('background','yellow');
+
+
+
+    //.ATTR
+    //Aqui fui até a minha tag 'personalizada' e encontrei o atributo que criei e mostrei o valor que ele possuia
+    //alert($('minhaTag').attr('meuattr'));
+
+    //Aqui fiz o mesmo caminho anterior, porém alterei o valor do atributo
+    //$('minhaTag').attr('meuattr','outroValor');
+
+    //Mostrando o valor recém adcionado
+    //alert($('minhaTag').attr('meuattr'));
+
+})
