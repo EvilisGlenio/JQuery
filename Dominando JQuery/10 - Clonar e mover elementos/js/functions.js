bbd8137e3e9a495539049cf48cc7c97d445a91e1
EvilisGlenio <glenio.developer@gmail.com>
2020-10-14 16:39:04 -0300

    Aula 10
    Finalizada

---
+++ Dominando JQuery/10 - Clonar e mover elementos/js/functions.js
@@ -0,0 +1,15 @@
+$(function(){
+    //CTRL C
+    /*.clone copia o conteúdo
+    var el = $('#source .texto').clone();
+
+    //CTRL V
+    //.appendTo cola  
+    el.appendTo('#container');*/
+
+    //CTRL X - CTRL V
+    //Recortando
+    $('#source .texto').appendTo('#container');
+
+
+})
