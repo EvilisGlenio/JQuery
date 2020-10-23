e70a400d71dcc75ab0d5f0af73542184430a0a7f
EvilisGlenio <glenio.developer@gmail.com>
2020-10-23 16:27:37 -0300

    Aula 14
    Finalizada

--- Dominando JQuery/14 - Finalizando validação/js/functions.js
+++ Dominando JQuery/14 - Finalizando validação/js/functions.js
@@ -1,5 +1,7 @@
 $(function(){
 
+    //Funções de abri e fechar formulário
+
     abrirJanela();
     verificarClickFechar();
 
@@ -23,14 +25,59 @@ $(function(){
         });
     }
 
+    //Eventos do formulário
+
+    $('input[type=text').focus(function(){
+        resetCampoInvalido($(this));
+    })
 
     $('#form1').submit(function(e){
-        e.preventDefault();
+        //e.preventDefault();
 
         var nome = $('input[name=nome]').val();
         var telefone = $('input[name=tel]').val();
         var email = $('input[name=email]').val();
         
+        if(validarNome(nome) == false){
+            aplicarCampoInvalido($('input[name=nome'));
+            return false;
+        }else if(validarTelefone(telefone) == false){
+            aplicarCampoInvalido($('input[name=tel]'));
+            return false;
+        }else if(validarEmail(email) == false){
+            aplicarCampoInvalido($('input[name=email]'))
+            return false;
+        }else{
+            alert("Formulário enviado com sucesso!!");
+        }
+       
+
+        //Se chegou aqui é pq ta Okay.
+    });
+
+
+    //Funções para estilizar o campo do formulário
+
+    function aplicarCampoInvalido(el){
+        el.css('color','red');
+        el.css('border','2px solid red');
+        el.val('Campo Inválido');
+        //el.data('invalido','true');
+    }
+    
+    function resetCampoInvalido(el){
+        el.css('color','#ccc');
+        el.css('border','1px solid #ccc');
+        el.val('');
+    }
+
+    //Funções para verificação dos campos
+
+    function validarNome(nome){
+        if(nome == ''){
+            return false;
+        }
+
         //Pega a quantidade de strings separadas por um espaço
         var amount = nome.split(' ').length;
         //Para pegar apenas os índicies(Palavras deparadas por um espaço)
@@ -42,22 +89,30 @@ $(function(){
                 if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
                     
                 }else{
-                    aplicarCampoInvalido($('input[name=nome]'));
+                    return false;
                 }
             }
         }else{
-            aplicarCampoInvalido($('input[name=nome]'));
             return false;
         }
+   }
 
-        //Se chegou aqui é pq ta Okay.
-    });
+   function validarTelefone(telefone){
+       if(telefone == ''){
+           return false;
+       }
+       if(telefone.match(/^\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/) == null){
+           return false;
+       }
+   }
 
-
-    function aplicarCampoInvalido(el){
-        el.css('border','2px solid red');
-        el.data('invalido','true');
-    }
-    
+   function validarEmail(email){
+       if(email == ''){
+           return false;
+       }
+       if(email.match(/^([A-Za-z0-9-_.]{1,})+@+([a-z.]{1,})$/) == null){
+           return false;
+       }
+   }
 
 });
