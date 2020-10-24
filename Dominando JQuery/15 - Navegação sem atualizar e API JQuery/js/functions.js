8d6bf519272660e8063ffc58b04e607419ce9f2c
EvilisGlenio <glenio.developer@gmail.com>
2020-10-24 16:25:16 -0300

    Aula 15
    Finalizada

--- Dominando JQuery/15 - Navegação sem atualizar e API JQuery/js/functions.js
+++ Dominando JQuery/15 - Navegação sem atualizar e API JQuery/js/functions.js
@@ -1,5 +1,30 @@
 $(function(){
 
-   
+   verificarClickMenu();
+
+   function verificarClickMenu(){
+       $('a').click(function(){
+           //Aqui a própria página q será clicada será guardada no href
+            var href = $(this).attr('href');
+            $.ajax({
+                'beforeSend':function(){
+                    console.log("Estamos chamando o beforeSend!");
+                },
+                'timeOut':10000,
+                'url':href,
+                'error':function(jqXHR,textStatus,errorThrown){
+                    if(jqXHR.statusText == 'Not Found'){
+                        console.log('Página não encontrada!');
+                    }
+                },
+                'success':function(data){
+                    $('#container').html(data);
+                    //$(data).appendTo('#container').fadeIn();
+                }
+
+            })
+            return false;
+       })
+   }
 
 });
