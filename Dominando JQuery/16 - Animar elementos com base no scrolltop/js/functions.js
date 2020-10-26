be9410339b5eaf1500d5993e00a14e65b61d0611
EvilisGlenio <glenio.developer@gmail.com>
2020-10-26 16:06:10 -0300

    Aula 16
    Finalizada

--- Dominando JQuery/16 - Animar elementos com base no scrolltop/js/functions.js
+++ Dominando JQuery/16 - Animar elementos com base no scrolltop/js/functions.js
@@ -1,5 +1,26 @@
 $(function(){
 
-  
-    
+  $(window).scroll(function(){
+      //Pega a posição da janela baseada na quantidade de pixels ocultos acima da mesma
+      var windowOffY = $(window).scrollTop();
+      //Obtem a altura da janela
+      var windowHeight = $(window).height();
+      
+    $('.sessao').each(function(){
+        //Obtem as coordenadas de um objeto
+        //Qual a distancia do topo do elemento até o topo da janela
+        var elOffY = $(this).offset().top;
+
+        if(elOffY+30 < (windowOffY + windowHeight) && elOffY+30+$(this).height > windowOffY){
+            //console.log("Estamos na sessão " + $(this).attr('target'));
+            $('a').css('border-bottom','0');
+
+            var target = $(this).attr('target');
+            $('.'+target).css('border-bottom','2px solid #333');
+
+            return;
+        }
+    });
+  });
+
 });
