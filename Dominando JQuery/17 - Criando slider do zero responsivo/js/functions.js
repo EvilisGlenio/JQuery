77b58d72c1751eb57acfe2e5d00df6b4d2657661
EvilisGlenio <glenio.developer@gmail.com>
2020-10-29 14:26:16 -0300

    Aula 17
    Finalizada

--- Dominando JQuery/17 - Criando slider do zero responsivo/js/functions.js
+++ Dominando JQuery/17 - Criando slider do zero responsivo/js/functions.js
@@ -2,25 +2,48 @@ $(function(){
 
   var indiceAtual = 0;
   var indiceMaximo = $('.slider img').length;
-  var delay = 3000;
+  var delay = 4000;
 
   
   initSlider();
-  console.log(indiceMaximo);
+  cliqueSlider();
 
   function initSlider(){
+    for( var i = 0; i < indiceMaximo; i++){
+      if(i == 0){
+        $('.bullets-nav').append('<span style="background-color:#069;"></span>');
+      }else{
+        $('.bullets-nav').append('<span></span>');
+      }
+    }
+
     $('.slider img').eq(0).fadeIn();
     setInterval(function(){
       alternarSlider();
     },delay);
   }
 
+  function cliqueSlider(){
+    $('.bullets-nav span').click(function(){
+      $('.slider img').eq(indiceAtual).stop().fadeOut(2000);
+        indiceAtual = $(this).index();
+        $('.slider img').eq(indiceAtual).stop().fadeIn(2000);
+
+        $('.bullets-nav span').css('background-color','#ccc');
+        $(this).css('background-color','#069');
+    });
+  }
+
   function alternarSlider(){
-    $('.slider img').eq(indiceAtual).fadeOut();
+    $('.slider img').eq(indiceAtual).stop().fadeOut(2000);
     indiceAtual+=1;
     if(indiceAtual == indiceMaximo){
       indiceAtual = 0;
     }
-    $('.slider img').eq(indiceAtual).fadeIn();
+
+    $('.bullets-nav span').css('background-color','#ccc');
+    $('.bullets-nav span').eq(indiceAtual).css('background-color','#069');
+
+    $('.slider img').eq(indiceAtual).stop().fadeIn(2000);
   }
 });
